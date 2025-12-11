a729e173ce6706852c2075e7d9cba3dd6d5c5a7a
iyeme-dev <iyemeaji@yahoo.com>
2025-12-11 13:41:43 +0000

    Connect database

--- envy.py
+++ envy.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgresql://neondb_owner:YOUR_PASSWORD@ep-flat-scene-agrmwosh.c-2.eu-central-1.aws.neon.tech/bloat_curse_swipe_942866"
 os.environ["SECRET_KEY"] = "django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg"
-os.environ["DEBUG"] = "True"  # or "False" for production
+os.environ["DEBUG"] = "False"  # or "False" for production
