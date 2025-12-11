732707701f29d5d47ebd70e723c15eb2fa791296
iyeme-dev <iyemeaji@yahoo.com>
2025-12-11 13:27:53 +0000

    Configure DATABASE_URL with dj_database_url and env.py

--- envy.py
+++ envy.py
@@ -1,4 +1,5 @@
 import os
 
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:npg_l7ncAFzHNjr9@ep-flat-scene-agrmwosh.c-2.eu-central-1.aws.neon.tech/bloat_curse_swipe_942866")
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:YOUR_PASSWORD@ep-flat-scene-agrmwosh.c-2.eu-central-1.aws.neon.tech/bloat_curse_swipe_942866"
+os.environ["SECRET_KEY"] = "django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg"
+os.environ["DEBUG"] = "True"  # or "False" for production
--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -19,15 +19,31 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# ...
+
+if os.environ.get("DATABASE_URL"):
+    # Use Postgres / Neon / Heroku DB when DATABASE_URL is set
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    # Fallback to SQLite locally
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg'
+# SECRET_KEY = 'django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -84,10 +100,6 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("postgresql://neondb_owner:npg_l7ncAFzHNjr9@ep-flat-scene-agrmwosh.c-2.eu-central-1.aws.neon.tech/bloat_curse_swipe_942866"))
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
