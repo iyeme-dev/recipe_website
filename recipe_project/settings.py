360a83f4995c71362fc04aeab7c594494a8961ee
iyeme-dev <iyemeaji@yahoo.com>
2025-12-11 13:47:45 +0000

    Update secret key

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -40,7 +40,7 @@ else:
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
