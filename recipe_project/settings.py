bae845412bdf25be0be02e5ea892c0811ffed5fc
iyeme-dev <iyemeaji@yahoo.com>
2025-12-11 11:02:27 +0000

    Readies code for deploymentment

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g0_y5rixo!!%_0y^d((axwsx4@-)xf=tf=mcsnp70ff!#cmveg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
